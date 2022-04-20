fce35ffefeb200a811f58a21e35e8f4dda670e31
Rooney-Eli <pianoman.rooney@gmail.com>
2022-04-19 22:39:43 -0600

    yolo

--- src/resources/static/html/library/library-script.js
+++ src/resources/static/html/library/library-script.js
@@ -44,14 +44,12 @@ window.addEventListener('load', () => {
     function getAudioSource(songId) {
         console.log("Getting src")
         console.log(songId)
-        for (const song in listOfSongs) {
-            console.log(song)
-            if (Number(song.songId) === Number(songId)) {
-                console.log(song.filepath)
-                return song.filepath
-            }
-        }
-        return "https://gorilla-tracks.s3.us-west-2.amazonaws.com/music/704.ogg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20220420T042258Z&X-Amz-SignedHeaders=host&X-Amz-Expires=14400&X-Amz-Credential=AKIAJIAKDDLEY424ICFA%2F20220420%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Signature=6672cd3a4e310cb6fae38c9869f86cc1c8c4755b1081a9dc4ecef32bc599d9bb"
+        const result = listOfSongs.find((song) => {
+            return songId === song.songId
+        })
+
+        console.log(result)
+        return result.filepath
     }
 
 })
