3ac669d7865ce6b9755c466af17838f3f22fee23
Rooney-Eli <pianoman.rooney@gmail.com>
2022-04-19 20:04:39 -0600

    yolo

--- index.php
+++ index.php
@@ -5,10 +5,10 @@ declare(strict_types = 1);
 require_once './src/php/ShufflrApp.php';
 require_once './src/php/Config.php';
 require_once './src/php/DotEnv.php';
-//require_once './src/php/di/Container.php';
+require_once './src/php/di/Container.php';
+require_once './src/php/RequestRouter.php';
 //require_once './src/php/controllers/LoginController.php';
 //require_once './src/php/controllers/LibraryController.php';
-//require_once './src/php/RequestRouter.php';
 //require_once './src/php/models/SongCacheEntity.php';
 //require_once './src/php/exceptions/RouteNotFoundException.php';
 //require_once './src/php/exceptions/ContainerException.php';
@@ -19,11 +19,11 @@ require_once './src/php/DotEnv.php';
 use Shufflrio\Src\ShufflrApp;
 use Shufflrio\Src\Php\Config;
 use Shufflrio\Src\Php\DotEnv;
+use Shufflrio\Src\Di\Container;
+use Shufflrio\Src\Php\RequestRouter;
 
 echo "Hello from post namespace use"; die();
-//use Shufflrio\Src\Di\Container;
 //use Shufflrio\Src\Php\Controllers\LibraryController;
-//use Shufflrio\Src\Php\RequestRouter;
 //use Shufflrio\Src\Php\Controllers\LoginController;
 //
 //
